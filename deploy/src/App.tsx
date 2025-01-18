2deb47da948aca188e973d3895b263aa7c0984a9
Mason Brown <masubrow@ucsc.edu>
2025-01-18 14:00:26 -0800

    Adding subfolders withinsrc and new imports

--- deploy/src/App.tsx
+++ deploy/src/App.tsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom"
+import { Container } from "react-bootstrap"
+import { Home } from "./pages/home"
+import { Store } from "./pages/store"
+import { About } from "./pages/about"
+import { Navbar } from "./components/Navbar"
+import { ShoppingCartProvider } from "./context/ShoppingCartContext"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>EcoShoppr</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Grocery Amount is {count}
-        </button>
-        <p>
-          Click the button pls
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Our site is WIP, check back later :)
-      </p>
-    </>
+    <ShoppingCartProvider>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </ShoppingCartProvider>
   )
 }
 
 export default App
+
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vite.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>EcoShoppr</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           Grocery Amount is {count}
+//         </button>
+//         <p>
+//           Click the button pls
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Our site is WIP, check back later :)
+//       </p>
+//     </>
+//   )
+// }
